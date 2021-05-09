aa1c10fce4e1a54b4d798b882c6f5c4490c66634
Irina Stepanova <irina.s.vesna@mail.ru>
2021-05-09 22:47:35 +0600

    feat: add coding function

--- utils/transform.js
+++ utils/transform.js
@@ -1,4 +1,5 @@
 const stream = require('stream');
+const code = require('./code');
 
 class CaesarTransform extends stream.Transform {
   constructor(action, shift) {
@@ -9,16 +10,19 @@ class CaesarTransform extends stream.Transform {
 
   _transform(data, encoding, callback) {
     let result = '';
+
     if (!Number.isInteger(Number(this.shift))) {
       process.stderr.write(`Error: Oops... Shift should be an integer!`);
       process.exit(1);
     }
+
     if (this.action === 'encode' || this.action === 'decode') {
-      result = data;
+      result = code(data, this.shift, this.action);
     } else {
       process.stderr.write('Error: Oops... Action not found!');
       process.exit(1);
     }
+
     this.push(result);
     this.push('\n');
     callback();
