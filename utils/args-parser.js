21779d0ea6225a45d1cfd2046d2599edbeaf7502
Irina Stepanova <irina.s.vesna@mail.ru>
2021-05-10 06:40:32 +0600

    feat: improve error handling

--- utils/args-parser.js
+++ utils/args-parser.js
@@ -11,8 +11,8 @@ program
 
 const options = program.opts();
 
-if (options.action !== 'decode' && options.action !== 'encode') {
-  process.stderr.write('Error: action should be either decode or encode!');
+if (options.action !== 'encode' && options.action !== 'decode') {
+  process.stderr.write('Error: action should be either encode or decode!');
   process.exit(1);
 }
 
@@ -24,23 +24,40 @@ if (!Number.isInteger(Number(options.shift))) {
 const inputFile = options.input ? path.resolve(options.input) : '';
 
 if (inputFile) {
-  fs.access(inputFile, fs.constants.F_OK | fs.constants.R_OK, (err) => {
+  try {
+    fs.accessSync(inputFile, fs.constants.F_OK | fs.constants.R_OK);
+  } catch (err) {
     if (err) {
-      process.stderr.write(`Input file ${err.code === 'ENOENT' ? 'does not exist' : 'is read-only'}.`);
+      process.stderr.write(`Input file ${err.code === 'ENOENT' ? 'does not exist' : 'cannot be read'}.`);
       process.exit(1);
     }
-  });
+  };
+  if (fs.lstatSync(inputFile).isDirectory()) {
+    process.stderr.write('Input file cannot be a directory.');
+    process.exit(1);
+  }
 }
 
 const outputFile = options.output ? path.resolve(options.output) : '';
 
 if (outputFile) {
-  fs.access(outputFile, fs.constants.F_OK | fs.constants.W_OK, (err) => {
+  if (outputFile === inputFile) {
+    process.stderr.write('Error: the input file cannot be equal to the output!');
+    process.exit(1);
+  }
+  try {
+    fs.accessSync(outputFile, fs.constants.F_OK | fs.constants.W_OK);
+  } catch (err) {
     if (err) {
       process.stderr.write(`Output file ${err.code === 'ENOENT' ? 'does not exist' : 'is read-only'}.`);
       process.exit(1);
     }
-  });
+  };
+  
+  if (fs.lstatSync(outputFile).isDirectory()) {
+    process.stderr.write('Output file cannot be a directory.');
+    process.exit(1);
+  }
 }
 
 const inputSrc = inputFile
